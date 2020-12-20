ea99ef545d096f857e0d88e84b268e5fe3a47657
Ivy <ivy83718@gmail.com>
2020-12-20 13:37:08 +0800

    SBP-7_fix_MEDIA_QUERY

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { theme } from "./theme";
 
-export const MEDIA_QUERY = "@media screen and (min-width: 768px)";
+export const MEDIA_QUERY = "@media screen and (max-width: 768px)";
 
 export const H1 = styled.h1`
   font-size: ${theme.fontSize.h1};
