02baad51a5d7e5e10894116ada3b6316db8af736
Ivy <ivy83718@gmail.com>
2020-12-20 13:33:29 +0800

    SBP-10_ProductList_Title

--- src/pages/user/ProductListPage/ProductListPage.js
+++ src/pages/user/ProductListPage/ProductListPage.js
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+import { MEDIA_QUERY, H1, Caption1 } from "../../../constants/style";
+const Content = styled.div`
+  margin: 75px 60px;
+`;
+
 export default function ProductListPage() {
-  return <div>PRODUCT_LIST</div>;
+  return (
+    <Content>
+      <H1>MENU</H1>
+      <Caption1>常溫蛋糕</Caption1>
+    </Content>
+  );
 }
