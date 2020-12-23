b79e282870d3a9ea4d6744cbefad0159a114a9dd
Core Kang <koshouhimeyasushi@Core-Kangs-MacBook-Pro.local>
2020-12-24 02:16:53 +0800

    SBP-9_pages/user/RegisterPage_v2

--- src/pages/user/RegisterPage/RegisterPage.js
+++ src/pages/user/RegisterPage/RegisterPage.js
@@ -12,21 +12,21 @@ const PageContainer = styled.div`
   justify-content: center;
 `;
 
-const LoginPageTitle = styled(H3)`
+const RegisterPageTitle = styled(H3)`
   margin-bottom: 30px;
   text-align: center;
 `;
 
-const LoginForm = styled.div`
+const RegisterForm = styled.div`
   width: 350px;
 `;
 
-const LoginInput = styled(Input)`
+const RegisterInput = styled(Input)`
   margin: 10px 0;
   width: 100%;
 `;
 
-const LoginButton = styled.button`
+const RegisterButton = styled.button`
   width: 100%;
   height: 46px;
   margin: 20px 0;
@@ -39,15 +39,15 @@ const LoginButton = styled.button`
 export default function RegisterPage() {
   return (
     <PageContainer>
-      <LoginPageTitle>加入會員</LoginPageTitle>
-      <LoginForm>
-        <LoginInput placeholder="帳號" />
-        <LoginInput placeholder="電子郵件" />
-        <LoginInput placeholder="手機號碼" />
-        <LoginInput type="password" placeholder="密碼" />
-        <LoginInput type="password" placeholder="再次輸入密碼" />
-        <LoginButton>註冊</LoginButton>
-      </LoginForm>
+      <RegisterPageTitle>加入會員</RegisterPageTitle>
+      <RegisterForm>
+        <RegisterInput placeholder="帳號" />
+        <RegisterInput placeholder="電子郵件" />
+        <RegisterInput placeholder="手機號碼" />
+        <RegisterInput type="password" placeholder="密碼" />
+        <RegisterInput type="password" placeholder="再次輸入密碼" />
+        <RegisterButton>註冊</RegisterButton>
+      </RegisterForm>
     </PageContainer>
   );
 }
