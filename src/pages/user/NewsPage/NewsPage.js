b5ed263e859fa9e810fadd09693539afea91b2fb
Core Kang <koshouhimeyasushi@Core-Kangs-MacBook-Pro.local>
2020-12-24 21:21:39 +0800

    SBP-12_pages/user/NewsPage_v1

--- src/pages/user/NewsPage/NewsPage.js
+++ src/pages/user/NewsPage/NewsPage.js
@@ -1,3 +1,50 @@
+import styled from "styled-components";
+import {
+  H3,
+  H5,
+  InputLine,
+  Textarea,
+  MEDIA_QUERY,
+} from "../../../constants/style";
+import { theme } from "../../../constants/theme";
+
+const PageContainer = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const PageTitle = styled(H3)`
+  margin-bottom: 30px;
+`;
+
+const NewsGroup = styled.div``;
+
+const NewsItem = styled.div``;
+
+const NewsItemImg = styled.div``;
+
+const NewsItemInfo = styled.div``;
+
 export default function NewsPage() {
-  return <div>NEWS</div>;
+  return (
+    <PageContainer>
+      <PageTitle>最新消息</PageTitle>
+      <NewsGroup>
+        <NewsItem>
+          <NewsItemImg></NewsItemImg>
+          <NewsItemInfo></NewsItemInfo>
+        </NewsItem>
+        <NewsItem>
+          <NewsItemImg></NewsItemImg>
+          <NewsItemInfo></NewsItemInfo>
+        </NewsItem>
+      </NewsGroup>
+    </PageContainer>
+  );
 }
