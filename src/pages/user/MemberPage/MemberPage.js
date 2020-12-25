c76e11925ee6c321bcbacae3c04a5fb3ad6e7e80
Core Kang <koshouhimeyasushi@Core-Kangs-MacBook-Pro.local>
2020-12-25 13:23:30 +0800

    SBP-16_pages/user/MemberPage_v2 react-bootstrap Accordion test

--- src/pages/user/MemberPage/MemberPage.js
+++ src/pages/user/MemberPage/MemberPage.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { H3, Input } from "../../../constants/style";
+import { H1, MEDIA_QUERY } from "../../../constants/style";
 import { theme } from "../../../constants/theme";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Accordion, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const PageContainer = styled.div`
+const PageContainer = styled(Container)`
   * {
     box-sizing: border-box;
   }
@@ -21,10 +24,46 @@ const PageContainer = styled.div`
 
 const PageTitle = styled(H1)``;
 
+const MemberCard = styled(Card)`
+  margin-top: 30px;
+`;
+
 export default function MemberPage() {
   return (
-    <PageContainer>
-      <PageTitle></PageTitle>
-    </PageContainer>
+    <Container>
+      <PageTitle>會員專區</PageTitle>
+      <Accordion>
+        <MemberCard>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              會員資料
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </MemberCard>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              消費紀錄
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="2">
+              我的最愛
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    </Container>
   );
 }
