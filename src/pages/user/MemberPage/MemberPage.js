11ce08c35211add44456e89f7175a2cc49578a36
Core Kang <koshouhimeyasushi@Core-Kangs-MacBook-Pro.local>
2020-12-25 10:19:51 +0800

    SBP-16_pages/user/MemberPage_v1 npm install --save react-bootstrap bootstrap

--- src/pages/user/MemberPage/MemberPage.js
+++ src/pages/user/MemberPage/MemberPage.js
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+import { H3, Input } from "../../../constants/style";
+import { theme } from "../../../constants/theme";
+
+const PageContainer = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+
+  max-width: 1280px;
+  margin: 40px auto;
+  padding: 0 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  ${MEDIA_QUERY} {
+    padding: 0 20px;
+  }
+`;
+
+const PageTitle = styled(H1)``;
+
 export default function MemberPage() {
-  return <div>MEMBER</div>;
+  return (
+    <PageContainer>
+      <PageTitle></PageTitle>
+    </PageContainer>
+  );
 }
