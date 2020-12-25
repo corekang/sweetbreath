6a53fd7434a5e69f962ea2a706a442786d2da353
Core Kang <koshouhimeyasushi@Core-Kangs-MacBook-Pro.local>
2020-12-25 18:36:12 +0800

    SBP-16_pages/user/MemberPage_v3

--- src/pages/user/MemberPage/MemberPage.js
+++ src/pages/user/MemberPage/MemberPage.js
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { H1, MEDIA_QUERY } from "../../../constants/style";
 import { theme } from "../../../constants/theme";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Accordion, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Tabs, Tab, Content } from "../../../components/Tab/Tab.js";
+import Table from "../../../components/Table/Table.js";
 
-const PageContainer = styled(Container)`
+const Container = styled.div`
   * {
     box-sizing: border-box;
   }
@@ -22,48 +23,119 @@ const PageContainer = styled(Container)`
   }
 `;
 
-const PageTitle = styled(H1)``;
+const PageTitle = styled(H1)`
+  margin-bottom: 40px;
+`;
 
-const MemberCard = styled(Card)`
-  margin-top: 30px;
+const TabGroup = styled.div`
+  width: 99%;
+  border: 1px solid #ccc;
+  border-top: none;
+  padding: 30px;
+`;
+
+const TabItem = styled.div`
+  display: column;
+  padding: 10px 0;
+  box-sizing: border-box;
+`;
+
+const EditButton = styled.button`
+  width: 50px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: ${theme.colors.mainPrimary};
+  border: 0;
+  color: #ffffff;
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  table {
+    width: 100%;
+    border: 1px solid black;
+    border-collapse: collapse;
+    th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+    th, td, tr {
+      padding: 10px;
+    }
+    th {
+      text-align: left;
+    }
+  }
 `;
 
 export default function MemberPage() {
+  const [active, setActive] = useState(0);
+  const handleClick = (e) => {
+    const index = parseInt(e.target.id, 0);
+    if (index !== active) {
+      setActive(index);
+    }
+  };
   return (
     <Container>
       <PageTitle>會員專區</PageTitle>
-      <Accordion>
-        <MemberCard>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              會員資料
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </MemberCard>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              消費紀錄
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="2">
-              我的最愛
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="2">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
+      <Tabs>
+        <Tab onClick={handleClick} active={active === 0} id={0}>
+          個人資料
+        </Tab>
+        <Tab onClick={handleClick} active={active === 1} id={1}>
+          消費紀錄
+        </Tab>
+        <Tab onClick={handleClick} active={active === 2} id={2}>
+          追蹤清單
+        </Tab>
+      </Tabs>
+      <>
+        <Content active={active === 0}>
+          <TabGroup>
+            <TabItem>姓名</TabItem>
+            <TabItem>電子郵件</TabItem>
+            <TabItem>
+              手機號碼<EditButton>變更</EditButton>
+            </TabItem>
+            <TabItem>
+              密碼<EditButton>變更</EditButton>
+            </TabItem>
+          </TabGroup>
+        </Content>
+        <Content active={active === 1}>
+          <TabGroup>
+            <GlobalStyle />
+            <Table />
+          </TabGroup>
+        </Content>
+        <Content active={active === 2}>
+          <TabGroup>
+            <TabItem>
+              商品名稱1<EditButton>購買</EditButton>
+              <EditButton>刪除</EditButton>
+            </TabItem>
+            <TabItem>
+              商品名稱2<EditButton>購買</EditButton>
+              <EditButton>刪除</EditButton>
+            </TabItem>
+          </TabGroup>
+        </Content>
+      </>
+      {/* <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs> */}
     </Container>
   );
 }
