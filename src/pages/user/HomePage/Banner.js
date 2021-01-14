2dcb51b5890ef22996f7e08fcaf6513105b2e216
Ivy <ivy83718@gmail.com>
2021-01-14 16:01:56 +0800

    compress_image

--- src/pages/user/HomePage/Banner.js
+++ src/pages/user/HomePage/Banner.js
@@ -23,19 +23,9 @@ export default function Banner() {
   return (
     <BannerContent>
       <Slider {...settings}>
-        <SliderItem
-          src={
-            "https://images.unsplash.com/photo-1562007908-69cf18a6da04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-          }
-          alt="pic1"
-        />
-        <SliderItem src={"https://imgur.com/HB8fY8K.jpg"} alt="pic2" />
-        <SliderItem
-          src={
-            "https://images.unsplash.com/photo-1601000937859-3031d1be1caa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-          }
-          alt="pic3"
-        />
+        <SliderItem src={"https://imgur.com/hD9sl8M.jpg"} alt="pic1" />
+        <SliderItem src={"https://imgur.com/3WznfZI.jpg"} alt="pic2" />
+        <SliderItem src={"https://imgur.com/SgjY53n.jpg"} alt="pic3" />
       </Slider>
     </BannerContent>
   );
